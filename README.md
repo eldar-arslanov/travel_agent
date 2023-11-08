46eba2c07d1d7e338b07da8273b37fcd579a8c47
Eldar <eldar.arslanov@gmail.com>
2023-11-08 15:20:42 +0500

    add link to readme.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project with GULP/JS/SCSS
+
+https://eldar-arslanov.github.io/travel_agent/
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+import autoCompleteFunc from './modules/autoComplete';
+autoCompleteFunc();
+
+// import easepick from './modules/easepick.js';
+// easepick();
+
+// import swiper from './modules/swiper.js';
+// swiper();
+
+// import scrollReveal from './modules/scrollReveal.js';
+// scrollReveal();
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
